7c68402a404686a10c2d3e1249938ba6167c0a27
chen barsano <chenbarsano@gmail.com>
2022-03-30 19:51:23 +0300

    added chat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import LoginForm from "./LoginForm/LoginForm";
+import Chat from "./Chat/Chat";
 
 function App() {
     return (
         <div className="App">
-            <LoginForm></LoginForm>
+            <Chat></Chat>
 
         </div>
     );
