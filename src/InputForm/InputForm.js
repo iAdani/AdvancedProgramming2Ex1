588c4dc894570acf871ee55ae0534b204de3e289
Guy Adani <guy.adani@live.biu.ac.il>
2022-03-30 16:04:28 +0300

    initialize commit

---
+++ src/InputForm/InputForm.js
@@ -0,0 +1,11 @@
+
+function InputForm({ placeholder, type, id }) {
+    return(
+        <div className="form-floating mb-3">
+            <input id={ id } type={ type } placeholder={ placeholder } className="form-control" ></input>
+            <label for={ id }>{ placeholder }</label>
+        </div>
+    )
+}
+
+export default InputForm;
---
+++ src/LoginForm/LoginForm.js
@@ -0,0 +1,16 @@
+import InputForm from "../InputForm/InputForm";
+import React from "react";
+
+function LoginForm() {
+    return(
+        <div>
+            <div className="loginFormText mb-3" > Sign in </div>
+            <InputForm id={ "login-email" } placeholder={ "Username" }></InputForm>
+            <InputForm id={ "login-pass" } type={ "password" } placeholder={ "Password" }></InputForm>
+            <button className="w-100 btn btn-primary">sign in</button>
+            <div className="mb-3 white-text">Not registered? <a href="/">Register</a>.</div>
+        </div>
+    )
+}
+
+export default LoginForm;
