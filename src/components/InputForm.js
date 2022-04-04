f4690a53852355aa610decc4979888d05e2050c7
Guy Adani <guy.adani@live.biu.ac.il>
2022-04-04 18:54:58 +0300

    Added DB adapter, initialized chat screen, added logic to login page

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,10 +1,37 @@
-function InputForm({ placeholder, type, id }) {
+import { useState } from "react";
+
+
+export default function InputForm({ placeholder, type, id }) {
+
+    const [valid, setValid] = useState("");
+
+    const validation = function() {
+        setValid(checkValidation(id))
+    }
+
     return(
         <div className="form-floating mb-3">
-            <input className="form-control styledInput" id={ id } type={ type } placeholder=" " />
-            <label className="hatula" htmlFor={ id }>{ placeholder }</label>
+            <input onKeyUp={ validation } className={ "form-control styledInput " + valid } id={ id } type={ type } placeholder=" " maxLength="12"/>
+            <label className="input-label-margin" htmlFor={ id }>{ placeholder }</label>
         </div>
     )
 }
 
-export default InputForm;
+function checkValidation(id) {
+    var input = document.getElementById(id).value;
+
+    /* --- Sign in page --- */
+    if(id === "login-username"){
+        if (input !== "") {return "goodInput"};
+        return "";
+    }
+
+    if(id === "login-password"){
+        if (input !== "") {return "goodInput"};
+        return "";
+    }
+
+    /* --- Register page --- */
+
+    
+}
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -2,19 +2,23 @@ import InputForm from "./InputForm";
 import React from "react";
 import {Link} from "react-router-dom";
 
-function RegisterForm() {
+export default function RegisterForm() {
+    const Register = function() {
+        var username = document.getElementById("login-username").value;
+        var password = document.getElementById("login-password").value;
+    }
+
     return(
         <div className="mainContainer">
             <div className="w-45">
                 <div className="welcomeText" >Register</div>
                 <InputForm id={ "register-username" } placeholder={ "Username" }> </InputForm>
-                <InputForm id={ "register-pass" } type={ "password" } placeholder={ "Password" }> </InputForm>
+                <InputForm id={ "register-password" } type={ "password" } placeholder={ "Password" }> </InputForm>
+                <InputForm id={ "register-pass-check" } type={ "password" } placeholder={ "Confirm Password" }> </InputForm>
                 <InputForm id={ "register-display-name" } type={ "text" } placeholder={ "Display Name" }> </InputForm>
-                <button className="mainButton w-100 btn btn-primary">Register</button>
+                <button onSubmit={Register} className="mainButton w-100 btn btn-primary" type="submit">Register</button>
                 <div className="mb-3 white-text">Already registered? <Link to="/login">Sign in</Link>.</div>
             </div>
         </div>
     )
 }
-
-export default RegisterForm;
