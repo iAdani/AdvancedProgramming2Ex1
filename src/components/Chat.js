6eb96cf387973f41e5086a014f048ef70061f1d4
Chen Barsano <chenbarsano@gmail.com>
2022-04-07 16:53:47 +0300

    Chatssss

---
+++ src/components/Chat.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Chat.css"
+
+function Chat() {
+  return (
+    <div className="chat">
+        <div className="chat__header">
+            <i class="btn bi bi-person-circle"></i>
+            <div className="chat__headerInfo">
+                <h4>Contact Name</h4>
+                <p>Last seen at...</p>
+            </div>
+        </div>
+
+        <div className="chat__body">
+            {/* only add chat__reciever if some condition is true-> message.name === user.displayName */}
+            <p className={`chat__message ${true &&'chat__reciever'}`}>Hello bitches
+                <span className="chat__timestamp">3:54pm</span>
+            </p>
+        </div>
+
+        <div className="chat__footer">
+            <h4>
+            <i class="bi bi-paperclip"></i>
+            </h4>
+            <form>
+                <input placeholder="Type a message" type="text" />
+                <button type="submit">Send a message</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Chat
