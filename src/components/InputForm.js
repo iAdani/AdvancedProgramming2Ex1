5ecdfe69318f18a80ba8e528346c8c2ce6f7d601
Guy Adani <guy.adani@live.biu.ac.il>
2022-04-15 05:16:49 +0300

    even more register validations

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,13 +1,13 @@
 import { useRef } from "react"
 
 
-export default function InputForm({ placeholder, type, id, setter }) {
+export default function InputForm({ placeholder, type, id, max, setter }) {
 
     const target = useRef("");
     
     return (
         <div className="form-floating mb-3">
-            <input onKeyUp={() => {setter(target.current.value);}} ref={target} className={"form-control styledInput"} id={id} type={type} placeholder=" " maxLength="12"/>
+            <input onKeyUp={() => {setter(target.current.value);}} ref={target} className={"form-control styledInput"} id={id} type={type} placeholder=" " maxLength={max}/>
             <label className="input-label-margin" htmlFor={id}>{placeholder}</label>
         </div>
     )
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,41 +1,79 @@
-import React, { useState } from "react";
-import {Link, useNavigate} from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import UsernameInput from "./RegisterInputs/UsernameInput";
 import PasswordInput from "./RegisterInputs/PasswordInput";
 import PassValidInput from "./RegisterInputs/PassValidInput";
 import DisplayInput from "./RegisterInputs/DisplayInput";
+import { AddUser } from "../DBAdapater";
+import $ from 'jquery';
+import { Overlay, Tooltip } from "react-bootstrap";
 
 export default function RegisterForm({ setActiveUser }) {
+    //used for registration
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [passValid, setPassValid] = useState('');
     const [nickname, setNickname] = useState('');
-
     let navigate = useNavigate();
 
-    const Register = function(element) {
+    // Used for submit button overlay
+    const [show, setShow] = useState(false);
+    const [submitOverlayText, setSubmitOverlayText] = useState('');
+    let buttonRef = useRef('');
+
+    // Used for submit button disabling feature
+    useEffect(() => {
+        if (username === '' || password === '' || passValid === '' || nickname === '') {
+            $('#registerSubmit').prop('disabled', true);
+        } else {
+            $('#registerSubmit').prop('disabled', false);
+        }
+    });
+
+    // Registering after submition
+    const Register = function (element) {
         element.preventDefault();
-        console.log(nickname);
         if (username !== '' && password !== '' && passValid !== '' && nickname !== '') {
+            AddUser(username, nickname, password);
             setActiveUser(username);
             navigate('/chats');
+        } else {
+            $('#registerContainer').addClass('red-border');
+            setTimeout(() => { $('#registerContainer').removeClass('red-border'); }, 4500);
         }
 
     }
 
-    return(
-        <div className="mainContainer overflow-hidden">
+    // Used for submit button overlay
+    const poppy = () => {
+        if ($('#registerSubmit').is(':disabled')) {
+            setSubmitOverlayText(<span className="badInput"><i className="bi bi-x-lg"></i> All fields must be valid!</span>);
+        } else {
+            setSubmitOverlayText(<span className="goodInput"><i className="bi bi-check-lg"></i> Good to go!</span>);
+        }
+        setShow(!show);
+    }
+
+    return (
+        <div id='registerContainer' className="mainContainer overflow-hidden">
             <form className="w-45" onSubmit={Register}>
                 <div className="welcomeText">Register</div>
-                <UsernameInput setter={ setUsername } />
-                <PasswordInput setter={ setPassword } />
-                <PassValidInput setter={ setPassValid } password={password} />
-                <DisplayInput setter={ setNickname } />
+                <UsernameInput setter={setUsername} />
+                <PasswordInput setter={setPassword} />
+                <PassValidInput setter={setPassValid} password={password} />
+                <DisplayInput setter={setNickname} />
                 <div className="mb-3 styledInput register-image pointer">
-                    <span className="pointer"><input id="register-image" type="file"/></span>
+                    <span className="pointer"><input id="register-image" type="file" /></span>
                     <label className="pointer" htmlFor="register-image"><i className="bi bi-cloud-arrow-up"></i> Upload an image</label>
                 </div>
-                <button className="mainButton w-75 btn" type="submit">Register</button>
+                <span id='submitSpan' ref={buttonRef} onMouseEnter={poppy} onMouseLeave={poppy} >
+                    <button id='registerSubmit' className="mainButton w-75 btn" type="submit">Register</button>
+                </span>
+                <Overlay target={buttonRef} show={show} placement="right">
+                {(props) => (
+                    <Tooltip {...props}><span>{submitOverlayText}</span></Tooltip>
+                )}
+            </Overlay>
                 <div className="mb-3 white-text">Already registered? <Link to="/login">Sign in</Link>.</div>
             </form>
         </div>
