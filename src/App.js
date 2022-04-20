213ccd1218db892f0e5eba271428471aab77d0f0
Guy Adani <guy.adani@live.biu.ac.il>
2022-04-20 06:26:47 +0300

    small fix - master must work perfectly

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { React, useState, createContext } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
 import LoginForm from "./components/Forms/LoginForm";
 import RegisterForm from "./components/Forms/RegisterForm";
 import MainScreen from "./components/Forms/MainScreen";
