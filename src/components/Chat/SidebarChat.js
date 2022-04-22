186d860cd4d1356dc4d2632852a0d3c488dadd6e
Guy Adani <guy.adani@live.biu.ac.il>
2022-04-22 19:07:30 +0300

    Completed image sending

--- src/components/Chat/SidebarChat.js
+++ src/components/Chat/SidebarChat.js
@@ -3,12 +3,23 @@ import "./SidebarChat.css";
 import { GetImage } from "../../DBAdapater";
 
 export default function SidebarChat(props) {
+
+  const lastMessage = (msg) => {
+    if (msg === undefined) return '';
+    if (msg.Type === "text") return <span>{msg.Content}</span>;
+    if (msg.Type === "image") {
+      return (
+       <span><i class="bi bi-card-image" /> Image</span>
+      )} 
+  }
+
+
   return (
     <div className="sidebarChat" onClick={() => props.setActiveContact(props.contact.toLowerCase())}>
         <img src={GetImage(props.contact)} />
         <div className="sidebarChat__info">
           <h2>{props.nickname}</h2>
-          <p>{props.lastMessage}</p>
+          <p>{lastMessage(props.lastMessage)}</p>
         </div>
     </div>
   );
